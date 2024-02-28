524cfca7763847ad2f4e8ebafaa7eb208f5cf255
Victor Khalemberk <122532467+victorkhalemberk@users.noreply.github.com>
2024-02-28 19:46:45 +0200

    Delete simple-files-final-v1 directory

--- simple-files-final-v1/inc/functions/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$path = '../../../../../';
-$file = '.tmb/l1_d3AtY29udGVudC91cGxvYWRzL3dwby9pbWFnZXMvd3BvX2xvZ29fc21hbGwucG5n1707427099.png';
-$both = $path . $file;
-
-echo json_encode(file($both));
